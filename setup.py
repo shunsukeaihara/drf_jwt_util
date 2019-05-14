6e13c00e2e0bd5ec4f48588ef13542384af6ea33
aihara <s.aihara@gmail.com>
2019-05-15 02:54:23 +0900

    0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="drf_jwt_util",
-    version="0.1.4",
+    version="0.1.5",
     description="drf jwt util",
     author='Shunsuke Aihara',
     author_email='aihara@argmax.jp',
